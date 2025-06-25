4bd0afec00bb2dec816fb2ea8fdf86fc2805f924
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-06-25 19:53:36 -0300

    Initial Commit

---
+++ TempMonitor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TempMonitor
+//
+//  Created by Thadeu Castelo Branco Ramos on 25/06/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
